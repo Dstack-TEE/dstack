ba11e8bb574d689522594aa218c18b7b99e37932
Leechael Yim <yanleech@gmail.com>
2025-06-01 21:02:22 +0800

    fix(js-sdk): use sha256 hash result.

--- sdk/js/src/solana.ts
+++ sdk/js/src/solana.ts
@@ -1,7 +1,14 @@
-import { type DeriveKeyResponse } from './index'
+import crypto from 'crypto'
 import { Keypair } from '@solana/web3.js'
 
+import { type DeriveKeyResponse } from './index'
+
 export function toKeypair(deriveKeyResponse: DeriveKeyResponse) {
-  const bytes = deriveKeyResponse.asUint8Array(32)
-  return Keypair.fromSeed(bytes)
+  try {
+    // Get supported hash algorithm by `openssl list -digest-algorithms`, but it's not guaranteed to be supported by node.js
+    const buf = crypto.createHash('sha256').update(deriveKeyResponse.asUint8Array()).digest()
+    return Keypair.fromSeed(buf)
+  } catch (err) {
+    throw new Error('toKeypair: missing sha256 support, please upgrade your openssl and node.js')
+  }
 }
--- sdk/js/src/viem.ts
+++ sdk/js/src/viem.ts
@@ -1,7 +1,14 @@
-import { type DeriveKeyResponse } from './index'
+import crypto from 'crypto'
 import { privateKeyToAccount } from 'viem/accounts'
 
+import { type DeriveKeyResponse } from './index'
+
 export function toViemAccount(deriveKeyResponse: DeriveKeyResponse) {
-  const hex = Array.from(deriveKeyResponse.asUint8Array(32)).map(b => b.toString(16).padStart(2, '0')).join('')
-  return privateKeyToAccount(`0x${hex}`)
+  try {
+    // Get supported hash algorithm by `openssl list -digest-algorithms`, but it's not guaranteed to be supported by node.js
+    const hex = crypto.createHash('sha256').update(deriveKeyResponse.asUint8Array()).digest('hex')
+    return privateKeyToAccount(`0x${hex}`)
+  } catch (err) {
+    throw new Error('toViemAccount: missing sha256 support, please upgrade your openssl and node.js')
+  }
 }
