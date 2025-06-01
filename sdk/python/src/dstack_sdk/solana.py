27cbfa89d59779570d19d056aac3f3e092004125
Leechael Yim <yanleech@gmail.com>
2025-06-01 21:53:12 +0800

    fix(python-sdk): use sha256 hash result.

--- sdk/python/src/dstack_sdk/solana.py
+++ sdk/python/src/dstack_sdk/solana.py
@@ -1,6 +1,8 @@
+import hashlib
 from solders.keypair import Keypair
 
 from .tappd_client import DeriveKeyResponse
 
 def to_keypair(derive_key_response: DeriveKeyResponse) -> Keypair:
-    return Keypair.from_seed(derive_key_response.toBytes(32))
+    hashed = hashlib.sha256(derive_key_response.toBytes()).digest()
+    return Keypair.from_seed(hashed)
