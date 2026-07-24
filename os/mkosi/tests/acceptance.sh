d4bcb6220156fea54b9a6b5b4885a6373c8dafd0
Kevin Wang <wy721@qq.com>
2026-07-24 10:23:29 -0700

    fix(os): retain mkosi lifecycle for release outputs

--- os/mkosi/tests/acceptance.sh
+++ os/mkosi/tests/acceptance.sh
@@ -11,6 +11,9 @@ source "$D/versions.env"
 [[ $KERNEL_VERSION == 7.* && $KERNEL_VERSION != *-rc* ]]
 [[ $KERNEL_SHA256 =~ ^[0-9a-f]{64}$ ]]
 grep -q "^Snapshot=$DEBIAN_SNAPSHOT" "$D/mkosi.conf"
+grep -q '^Format=tar$' "$D/mkosi.conf"
+grep -q '^Output=dstack-0.6.0$' "$D/mkosi.conf"
+grep -q '^CompressOutput=gz$' "$D/mkosi.conf"
 grep -q '^Bootable=no$' "$D/mkosi.conf"
 grep -q '^Bootloader=none$' "$D/mkosi.conf"
 for key in ACPI ACPI_TABLE_UPGRADE INTEL_TDX_GUEST TDX_GUEST_DRIVER \
