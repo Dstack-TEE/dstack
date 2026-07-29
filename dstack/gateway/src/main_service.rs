217996ca9da01afd0cbf1b5c61924934546d4a96
Kevin Wang <wy721@qq.com>
2026-07-29 11:03:36 +0000

    feat(gateway): configure app-address DNS server

--- dstack/gateway/src/main_service.rs
+++ dstack/gateway/src/main_service.rs
@@ -323,6 +323,7 @@ impl ProxyInner {
             AppAddressResolver::new(
                 config.proxy.app_address_ns_prefix.clone(),
                 config.proxy.app_address_ns_compat,
+                config.proxy.app_address_dns_server,
             )
             .context("failed to create app address resolver")?,
         );
